30bc7d6c74e3833d1285ce2fb1305e8427537f34
gayanehakobyan <gayanehakobyan93@gmail.com>
2018-11-30 23:52:01 +0400

    react version

---
+++ FetchRequest-React/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Fetch from "./FetchRequest/Fetch.js"
+
+
+
+const app=document.getElementById("root")
+ReactDOM.render(<Fetch />, app)
