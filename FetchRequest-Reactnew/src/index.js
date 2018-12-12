2f80c434795c43015b54cee0efc8db2f0eced592
gayanehakobyan <44981467+gayanehakobyan@users.noreply.github.com>
2018-12-12 22:15:19 +0400

    Delete index.js

--- FetchRequest-Reactnew/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Fetch from './FetchRequest/Fetch.js'
-
-
-
-const app=document.getElementById("root")
-ReactDOM.render(<Fetch />, app)
